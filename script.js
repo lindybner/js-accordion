cf8ea25b924fdd343171d651171bcec43e4a86cf
Jim Lin-Dybner <lin.jycl@gmail.com>
2024-03-10 09:53:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 window.onload = () => {
   // VARIABLES
   const accordionHeading = document.querySelector(".accordion-container");
-  const accordionContent = document.querySelector("#accordion-content");
+  const accordionContent = document.querySelector(".accordion-content");
 
   // FUNCTIONS
   // hide accordionContent on load
   const hideContent = () => {
     accordionContent.classList.add("hideElement");
   };
+
+  hideContent();
 };
